78b1dac79c730052746890de6711ad28cb38318b
Terence Faid JABO <faidterence@outlook.com>
2024-02-28 11:10:44 +0200

    order status update

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,7 +1,14 @@
-import { BadRequestException, Body, Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+} from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { StripeService } from 'src/stripe/stripe.service';
 import { ApiTags } from '@nestjs/swagger';
+import { OrderService } from 'src/order/order.service';
 
 @ApiTags('Stripe Payment')
 @Controller('payment')
@@ -9,14 +16,25 @@ export class PaymentController {
   constructor(
     private readonly paymentService: PaymentService,
     private readonly stripeService: StripeService,
+    private readonly orderService: OrderService,
   ) {}
 
   // Create a new payment with stripe
 
-  @Get('/success')
-  async paymentSuccessfullMessage() {
-    return {
-      message: 'Payment Successfull',
-    };
+  @Get('/success/:orderId')
+  async paymentSuccessfullMessage(@Param('orderId') orderId: number) {
+    try {
+      const status = 'completed';
+      await this.orderService.updateOrderStatus(orderId, status);
+
+      return {
+        message: 'Payment Successful. Order status updated to completed.',
+      };
+    } catch (error) {
+      return {
+        message: 'Failed to update order status.',
+        error: error.message,
+      };
+    }
   }
 }
