bf9c9759df31e7e00fa3b50c781a3fc18a8b6b54
Terence Faid JABO <faidterence@outlook.com>
2024-03-16 22:00:20 +0200

    feat(messages):create functionality

---
+++ src/messages/DTO/create-message.dto.ts
@@ -0,0 +1,5 @@
+export default class CreateMessageDto {
+  userEmail: string;
+  messageSubject: string;
+  message: string;
+}
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import Messages from './schema/messages.entity';
+import { Repository } from 'typeorm';
+import CreateMessageDto from './DTO/create-message.dto';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectRepository(Messages)
+    private messagesRepository: Repository<Messages>,
+  ) {}
+
+  async userSendMessage(message: CreateMessageDto) {
+    try {
+      const newMessage = await this.messagesRepository.create(message);
+      await this.messagesRepository.save(newMessage);
+      return {
+        message: 'Message sent successfully!',
+        newMessage,
+      };
+    } catch (error) {
+      throw new Error(`Error: ${error.message}`);
+    }
+  }
+}
